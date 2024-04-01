f0d72bd6a000a54e52c2abf19ffd95a75b729a60
Evgeny Mamaev <dsalion44@gmail.com>
2024-04-01 12:44:52 +0500

    добавлены темплейты, созданы функции

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,37 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "фотографии Архыза",
+      id: "1",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "фотографии Челябинской области",
+      id: "2",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "фотографии Иваново",
+      id: "3",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "фотографии Камчатки",
+      id: "4",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "фотографии Холмогорского района",
+      id: "5",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "фотографии Байкала",
+      id: "6",      
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
+const placesList = document.querySelector('.places__list');
+// @todo: Функция создания карточки: клонировать шаблон, установить значение элементов, добавить к иконке удаления обработчик клика, по которому будет вызван переданный в аргументах колбэк.
+function createCard(card, removeHandler) {
+   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+   cardElement.querySelector('.card__image').src = card.link;
+   cardElement.querySelector('.card__image').alt = card.alt;
+   cardElement.querySelector('.card__title').textContent = card.name;
+   cardElement.setAttribute('id', card.id);
+   const removeButton = cardElement.querySelector('.card__delete-button');
+   removeButton.addEventListener('click', removeHandler);
+   removeButton.dataset.id= card.id; 
+   return cardElement;
+ };
 // @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function removeHandler (event) {
+const getId = event.target.getAttribute('data-id');
+document.getElementById(getId).remove();
+};
+// @todo: Вывести карточки на страницу: 
+initialCards.forEach(function(item) { placesList.append(createCard(item,removeHandler));
+});
