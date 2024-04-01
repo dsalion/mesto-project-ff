18680ef91c0858667d008af728b547719221276b
Evgeny Mamaev <dsalion44@gmail.com>
2024-04-01 13:35:57 +0500

    fix после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,18 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки: клонировать шаблон, установить значение элементов, добавить к иконке удаления обработчик клика, по которому будет вызван переданный в аргументах колбэк.
 function createCard(card, removeHandler) {
    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-   cardElement.querySelector('.card__image').src = card.link;
-   cardElement.querySelector('.card__image').alt = card.alt;
-   cardElement.querySelector('.card__title').textContent = card.name;
-   cardElement.setAttribute('id', card.id);
+   const cardImage = cardElement.querySelector('.card__image');
+   const cardTitle = cardElement.querySelector('.card__title');
+   cardImage.src = card.link;
+   cardImage.alt = card.alt;
+   cardTitle.textContent = card.name;
    const removeButton = cardElement.querySelector('.card__delete-button');
-   removeButton.addEventListener('click', removeHandler);
-   removeButton.dataset.id= card.id; 
+   removeButton.addEventListener('click', () => removeHandler(cardElement)); 
    return cardElement;
  };
 // @todo: Функция удаления карточки
-function removeHandler (event) {
-const getId = event.target.getAttribute('data-id');
-document.getElementById(getId).remove();
+function removeHandler (cardElement) {
+cardElement.remove();
 };
 // @todo: Вывести карточки на страницу: 
 initialCards.forEach(function(item) { placesList.append(createCard(item,removeHandler));
